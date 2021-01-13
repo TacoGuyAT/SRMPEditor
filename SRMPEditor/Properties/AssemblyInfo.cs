67bb73e4e594ab5a92643cebe592c13347201f1c
TacoGuyAT <apotroend@outlook.com>
2021-01-14 01:50:55 +0200

    Cleanup + AssemblyInfo

--- SRMPEditor/Properties/AssemblyInfo.cs
+++ SRMPEditor/Properties/AssemblyInfo.cs
@@ -8,13 +8,14 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SRMPEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Slime Rancher Multiplayer World Files Editor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("__tacoguy")]
 [assembly: AssemblyProduct("SRMPEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -51,5 +52,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
